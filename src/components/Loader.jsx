9da6cc281a154b724b7eb0d99721f1efd874859e
Sushil Varande <37429804+sushil15@users.noreply.github.com>
2021-08-04 18:27:39 +0530

    Add files via upload

---
+++ src/components/Loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import loader from '../images/loader.gif';
+const Loader=()=>{
+    return(
+        <div className="container loader">
+            <div className="row">
+                <div className="col-12 loader_div">
+                  <img src={loader} className="img-fluid" alt="loading"></img>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Loader;
